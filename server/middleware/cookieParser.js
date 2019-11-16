ed989af29be17ebff03d40a3175aed8b1fe1a972
impeter9 <impeter9@gmail.com>
2019-11-15 20:07:40 -0800

    Nomming on fully parsed cookies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,20 @@
 const parseCookies = (req, res, next) => {
+
+  let cookieMonster = {};
+  if (req.headers.cookie) {
+    if (req.headers.cookie.includes(';')) {
+      let chocolateChipCookie = req.headers.cookie.split('; ');
+      chocolateChipCookie.forEach(cookie => {
+        let snickerdoodle = cookie.split('=');
+        cookieMonster[snickerdoodle[0]] = snickerdoodle[1];
+      });
+    } else {
+      let snickerdoodle = req.headers.cookie.split('=');
+      cookieMonster[snickerdoodle[0]] = snickerdoodle[1];
+    }
+  }
+  req.cookies = cookieMonster;
+  next();
 };
 
 module.exports = parseCookies;
