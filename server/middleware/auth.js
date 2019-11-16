b22eee78941361c91fef3840e08b6c83ae39ba71
impeter9 <impeter9@gmail.com>
2019-11-16 14:30:04 -0800

    finish session parser kind of

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,28 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  
+  if (req.cookies.shortlyid) {
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+      .then((result) => {
+        req.session = result;
+        next();
+      });
+  } else {
+    models.Sessions.create()
+      .then((session) => {
+        models.Sessions.get({ id: session.insertId })
+          .then((sessionData) => {
+            // console.log(sessionData);
+            req.session = sessionData;
+            res.cookies['shortlyid'] = { value: sessionData.hash };
+            next();
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        next();
+      });
+  }
 };
 
 /************************************************************/
